c511ff664003ba5d35f0b456dc65075950796c85
xusheng <xusheng@cc.com>
2024-03-06 22:22:23 +0800

    hot-fix commit

--- src/com/xzc/test/gitTest.java
+++ src/com/xzc/test/gitTest.java
@@ -8,5 +8,6 @@ package com.xzc.test;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git01");
+        System.out.println("Hello Git02");
     }
 }
