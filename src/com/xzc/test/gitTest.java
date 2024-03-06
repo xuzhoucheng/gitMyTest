2c4b1b11612d5f0aa51e3e308b45715033e2a88f
xusheng <xusheng@cc.com>
2024-03-06 22:25:51 +0800

    master commit

--- src/com/xzc/test/gitTest.java
+++ src/com/xzc/test/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git01");
         System.out.println("Hello Git02");
+        System.out.println("master test");
     }
 }
