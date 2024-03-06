c956096dd38a4350d320190d67bdcf610dce7472
xusheng <xusheng@cc.com>
2024-03-06 22:59:42 +0800

    push commit

--- src/com/xzc/test/gitTest.java
+++ src/com/xzc/test/gitTest.java
@@ -11,5 +11,6 @@ public class gitTest {
         System.out.println("Hello Git02");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
