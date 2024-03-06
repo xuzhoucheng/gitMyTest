22e728e863afdac863c3a61647b6142d6b0b7562
xusheng <xusheng@cc.com>
2024-03-06 22:21:38 +0800

    master first commit

---
+++ src/com/xzc/test/gitTest.java
@@ -0,0 +1,12 @@
+package com.xzc.test;
+
+/**
+ * @author 徐生
+ * @version 1.0
+ * @date 2024/3/6 22:20
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git01");
+    }
+}
